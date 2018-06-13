886c9c6a3d79a0ac213ad1ca105e608077952e74
dawiddc <dawid.ciok@gmail.com>
2018-06-13 11:01:07 +0200

    Add difficulty levels

--- PUTballGame/Assets/scripts/GameManager.cs
+++ PUTballGame/Assets/scripts/GameManager.cs
@@ -1,110 +1,166 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-	public delegate void GameDelegate();
-	public static event GameDelegate OnGameStarted;
-	public static event GameDelegate OnGameOverConfirmed;
+    public delegate void GameDelegate();
+    public static event GameDelegate OnGameStarted;
+    public static event GameDelegate OnGameOverConfirmed;
 
-	public static GameManager Instance;
+    public static GameManager Instance;
 
-	public GameObject startPage;
-	public GameObject gameOverPage;
-	public GameObject countdownPage;
-	public Text scoreText;
+    public GameObject startPage;
+    public GameObject gameOverPage;
+    public GameObject countdownPage;
+    public GameObject chooseDifficultyPage;
+    public Text scoreText;
+    public static GameDifficulty gameDifficulty;
 
-	enum PageState {
-		None,
-		Start,
-		Countdown,
-		GameOver
-	}
+    enum PageState
+    {
+        None,
+        Start,
+        Countdown,
+        GameOver,
+        ChooseDifficulty
+    }
 
-	int score = 0;
-	bool gameOver = true;
+    public enum GameDifficulty
+    {
+        Easy,
+        Medium,
+        Hard
+    }
 
-	public bool GameOver { get { return gameOver; } }
+    int score = 0;
+    bool gameOver = true;
 
-	void Awake() {
-		if (Instance != null) {
-			Destroy(gameObject);
-		}
-		else {
-			Instance = this;
-			DontDestroyOnLoad(gameObject);
-		}
-	}
+    public bool GameOver { get { return gameOver; } }
 
-	void OnEnable() {
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void OnEnable()
+    {
         scoreText.enabled = false;
         TapController.OnPlayerDied += OnPlayerDied;
-		TapController.OnPlayerScored += OnPlayerScored;
-		CountdownText.OnCountdownFinished += OnCountdownFinished;
-	}
+        TapController.OnPlayerScored += OnPlayerScored;
+        CountdownText.OnCountdownFinished += OnCountdownFinished;
+    }
 
-	void OnDisable() {
-		TapController.OnPlayerDied -= OnPlayerDied;
-		TapController.OnPlayerScored -= OnPlayerScored;
-		CountdownText.OnCountdownFinished -= OnCountdownFinished;
-	}
+    void OnDisable()
+    {
+        TapController.OnPlayerDied -= OnPlayerDied;
+        TapController.OnPlayerScored -= OnPlayerScored;
+        CountdownText.OnCountdownFinished -= OnCountdownFinished;
+    }
 
-	void OnCountdownFinished() {
-		SetPageState(PageState.None);
-		OnGameStarted();
-		score = 0;
-		gameOver = false;
-	}
+    void OnCountdownFinished()
+    {
+        SetPageState(PageState.None);
+        Parallaxer.ConfigureDifficulty();
+        OnGameStarted();
+        score = 0;
+        gameOver = false;
+    }
 
-	void OnPlayerScored() {
-		score++;
-		scoreText.text = score.ToString();
-	}
+    void OnPlayerScored()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
 
-	void OnPlayerDied() {
-		gameOver = true;
-		int savedScore = PlayerPrefs.GetInt("HighScore");
-		if (score > savedScore) {
-			PlayerPrefs.SetInt("HighScore", score);
-		}
+    void OnPlayerDied()
+    {
+        gameOver = true;
+        int savedScore = PlayerPrefs.GetInt("HighScore");
+        if (score > savedScore)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
         scoreText.enabled = false;
-		SetPageState(PageState.GameOver);
-	}
+        SetPageState(PageState.GameOver);
+    }
 
-	void SetPageState(PageState state) {
-		switch (state) {
-			case PageState.None:
-				startPage.SetActive(false);
-				gameOverPage.SetActive(false);
-				countdownPage.SetActive(false);
-				break;
-			case PageState.Start:
-				startPage.SetActive(true);
-				gameOverPage.SetActive(false);
-				countdownPage.SetActive(false);
-				break;
-			case PageState.Countdown:
-				startPage.SetActive(false);
-				gameOverPage.SetActive(false);
-				countdownPage.SetActive(true);
-				break;
-			case PageState.GameOver:
-				startPage.SetActive(false);
-				gameOverPage.SetActive(true);
-				countdownPage.SetActive(false);
-				break;
-		}
-	}
-	
-	public void ConfirmGameOver() {
-		SetPageState(PageState.Start);
-		scoreText.text = "0";
-		OnGameOverConfirmed();
-	}
+    void SetPageState(PageState state)
+    {
+        switch (state)
+        {
+            case PageState.None:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(false);
+                chooseDifficultyPage.SetActive(false);
+                break;
+            case PageState.Start:
+                startPage.SetActive(true);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(false);
+                chooseDifficultyPage.SetActive(false);
+                break;
+            case PageState.Countdown:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(true);
+                chooseDifficultyPage.SetActive(false);
+                break;
+            case PageState.GameOver:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(true);
+                countdownPage.SetActive(false);
+                chooseDifficultyPage.SetActive(false);
+                break;
+            case PageState.ChooseDifficulty:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(false);
+                chooseDifficultyPage.SetActive(true);   
+                break;
+        }
+    }
 
-	public void StartGame() {
+    public void ConfirmGameOver()
+    {
+        SetPageState(PageState.Start);
+        scoreText.text = "0";
+        OnGameOverConfirmed();
+    }
+
+    public void ChooseDifficulty()
+    {
+        SetPageState(PageState.ChooseDifficulty);
+    }
+
+    public void ChooseEasyDifficulty()
+    {
+        gameDifficulty = GameDifficulty.Easy;
+        StartGame();
+    }
+    public void ChooseMediumDifficulty()
+    {
+        gameDifficulty = GameDifficulty.Medium;
+        StartGame();
+    }
+    public void ChooseHardDifficulty()
+    {
+        gameDifficulty = GameDifficulty.Hard;
+        StartGame();
+    }
+
+    public void StartGame()
+    {
         scoreText.enabled = true;
         SetPageState(PageState.Countdown);
-	}
-
+    }
 }
--- PUTballGame/Assets/scripts/Parallaxer.cs
+++ PUTballGame/Assets/scripts/Parallaxer.cs
@@ -2,148 +2,189 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parallaxer : MonoBehaviour {
+public class Parallaxer : MonoBehaviour
+{
 
-	class PoolObject {
-		public Transform transform;
-		public bool inUse;
-		public PoolObject(Transform t) { transform = t; }
-		public void Use() { inUse = true; }
-		public void Dispose() { inUse = false; }
-	}
+    class PoolObject
+    {
+        public Transform transform;
+        public bool inUse;
+        public PoolObject(Transform t) { transform = t; }
+        public void Use() { inUse = true; }
+        public void Dispose() { inUse = false; }
+    }
 
-	[System.Serializable]
-	public struct XSpawnRange {
-		public float minX;
-		public float maxX;
-	}
+    [System.Serializable]
+    public struct XSpawnRange
+    {
+        public float minX;
+        public float maxX;
+    }
 
-	public GameObject Prefab;
-	public int poolSize;
-	public float shiftSpeed;
-	public float spawnRate;
+    public GameObject Prefab;
+    public int poolSize;
+    public static float shiftSpeed;
+    public static float spawnRate;
 
-	public XSpawnRange xSpawnRange;
-	public Vector3 defaultSpawnPos;
-	public bool spawnImmediate;
-	public Vector3 immediateSpawnPos;
-	public Vector2 targetAspectRatio;
+    public XSpawnRange xSpawnRange;
+    public Vector3 defaultSpawnPos;
+    public bool spawnImmediate;
+    public Vector3 immediateSpawnPos;
+    public Vector2 targetAspectRatio;
 
-	float spawnTimer;
-	PoolObject[] poolObjects;
-	float targetAspect;
-	GameManager game;
+    float spawnTimer;
+    PoolObject[] poolObjects;
+    float targetAspect;
+    GameManager game;
 
-	void Awake() {
-		Configure();
-	}
+    void Awake()
+    {
+        Configure();
+    }
 
-	void Start() {
-		game = GameManager.Instance;
-	}
+    public static void ConfigureDifficulty()
+    {
+        if (GameManager.gameDifficulty == GameManager.GameDifficulty.Easy)
+        {
+            shiftSpeed = -1.5f;
+            spawnRate = 1.1f;
+        } else if (GameManager.gameDifficulty == GameManager.GameDifficulty.Medium)
+        {
+            shiftSpeed = -2.0f;
+            spawnRate = 0.8f;
+        } else
+        {
+            shiftSpeed = -2.5f;
+            spawnRate = 0.65f;
+        }
+    }
 
-	void OnEnable() {
-		GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
-	}
+    void Start()
+    {
+        game = GameManager.Instance;
+    }
 
-	void OnDisable() {
-		GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
-	}
+    void OnEnable()
+    {
+        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
+    }
 
-	void OnGameOverConfirmed() {
-		for (int i = 0; i < poolObjects.Length; i++) {
-			poolObjects[i].Dispose();
-			poolObjects[i].transform.position = Vector3.one * 1000;
-		}
-		Configure();
-	}
+    void OnDisable()
+    {
+        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
+    }
+
+    void OnGameOverConfirmed()
+    {
+        for (int i = 0; i < poolObjects.Length; i++)
+        {
+            poolObjects[i].Dispose();
+            poolObjects[i].transform.position = Vector3.one * 1000;
+        }
+        Configure();
+    }
 
     private bool startSpawn = false;
 
-	void Update() {
+    void Update()
+    {
         if (game.GameOver) return;
 
-        if(!startSpawn)
+        if (!startSpawn)
         {
             Spawn();
             startSpawn = true;
         }
-        
+
         Shift();
-		spawnTimer += Time.deltaTime;
-        if (spawnTimer > spawnRate) {
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer > spawnRate)
+        {
+            Debug.Log(shiftSpeed + ", " + spawnRate);
             Spawn();
-			spawnTimer = 0;
-		}
-	}
+            spawnTimer = 0;
+        }
+    }
 
-	void Configure() {
-		//spawning pool objects
-		targetAspect = targetAspectRatio.x / targetAspectRatio.y;
-		poolObjects = new PoolObject[poolSize];
-		for (int i = 0; i < poolObjects.Length; i++) {
-			GameObject go = Instantiate(Prefab) as GameObject;
-			Transform t = go.transform;
-			t.SetParent(transform);
-			t.position = Vector3.one * 1000;
-			poolObjects[i] = new PoolObject(t);
-		}
+    void Configure()
+    {
+        //spawning pool objects
+        targetAspect = targetAspectRatio.x / targetAspectRatio.y;
+        poolObjects = new PoolObject[poolSize];
+        for (int i = 0; i < poolObjects.Length; i++)
+        {
+            GameObject go = Instantiate(Prefab) as GameObject;
+            Transform t = go.transform;
+            t.SetParent(transform);
+            t.position = Vector3.one * 1000;
+            poolObjects[i] = new PoolObject(t);
+        }
 
-		if (spawnImmediate) {
-			SpawnImmediate();
-		}
-	}
+        if (spawnImmediate)
+        {
+            SpawnImmediate();
+        }
+    }
 
-	void Spawn() {
-		//moving pool objects into place
-		Transform t = GetPoolObject();
+    void Spawn()
+    {
+        //moving pool objects into place
+        Transform t = GetPoolObject();
         if (t == null)
         {
             return;
         }
         Vector3 pos = Vector3.zero;
-		pos.x = Random.Range(xSpawnRange.minX, xSpawnRange.maxX);
-		pos.y = (defaultSpawnPos.y * Camera.main.aspect) / targetAspect;
-		t.position = pos;
-	}
-
-	void SpawnImmediate() {
-		Transform t = GetPoolObject();
-		if (t==null) return;
-		Vector3 pos = Vector3.zero;
         pos.x = Random.Range(xSpawnRange.minX, xSpawnRange.maxX);
         pos.y = (defaultSpawnPos.y * Camera.main.aspect) / targetAspect;
-        t.position = pos; 
-		Spawn();
-	}
+        t.position = pos;
+    }
 
-	void Shift() {
-		//loop through pool objects 
-		//moving them
-		//discarding them as they go off screen
-		for (int i = 0; i < poolObjects.Length; i++) {
+    void SpawnImmediate()
+    {
+        Transform t = GetPoolObject();
+        if (t == null) return;
+        Vector3 pos = Vector3.zero;
+        pos.x = Random.Range(xSpawnRange.minX, xSpawnRange.maxX);
+        pos.y = (defaultSpawnPos.y * Camera.main.aspect) / targetAspect;
+        t.position = pos;
+        Spawn();
+    }
+
+    void Shift()
+    {
+        //loop through pool objects 
+        //moving them
+        //discarding them as they go off screen
+        for (int i = 0; i < poolObjects.Length; i++)
+        {
             poolObjects[i].transform.position += Vector3.down * shiftSpeed * Time.deltaTime;
-			CheckDisposeObject(poolObjects[i]);
-		}
-	}
+            CheckDisposeObject(poolObjects[i]);
+        }
+    }
 
-	void CheckDisposeObject(PoolObject poolObject) {
-		//place objects off screen
-		if (poolObject.transform.position.y > (-defaultSpawnPos.y * Camera.main.aspect) / targetAspect) {
-			poolObject.Dispose();
-			poolObject.transform.position = Vector3.one * 1000;
-		}
-	}
+    void CheckDisposeObject(PoolObject poolObject)
+    {
+        //place objects off screen
+        if (poolObject.transform.position.y > (-defaultSpawnPos.y * Camera.main.aspect) / targetAspect)
+        {
+            poolObject.Dispose();
+            poolObject.transform.position = Vector3.one * 1000;
+        }
+    }
 
-	Transform GetPoolObject() {
-		//retrieving first available pool object
-		for (int i = 0; i < poolObjects.Length; i++) {
-			if (!poolObjects[i].inUse) {
-				poolObjects[i].Use();
-				return poolObjects[i].transform;
-			}
-		}
-		return null;
-	}
+    Transform GetPoolObject()
+    {
+        //retrieving first available pool object
+        for (int i = 0; i < poolObjects.Length; i++)
+        {
+            if (!poolObjects[i].inUse)
+            {
+                poolObjects[i].Use();
+                return poolObjects[i].transform;
+            }
+        }
+        return null;
+    }
 
 }
